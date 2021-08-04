0a32089053dbdcc73609474854ba93d9cce929da
Conner Ward <ckward@uci.edu>
2021-08-03 19:52:23 -0700

    clear

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,7 @@ var Homepage = (props) => {
     return (
         <div className="main">
             <Headline props={{ title: "Welcome", subtitle: "Hi... you!" }}></Headline>
-            <p> Ex labore enim exercitation non id fugiat mollit mollit sunt aliqua. Minim duis sint ex cillum consequat reprehenderit reprehenderit. Cupidatat duis ad pariatur eu enim officia commodo exercitation irure aliqua. Sit est consequat commodo culpa anim magna mollit culpa occaecat duis et.
-            </p>
+
         </div>
     );
 
