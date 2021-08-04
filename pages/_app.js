7d0c5c48e0f1f31ad54c3161af1f5476353240ce
Conner Ward <ckward@uci.edu>
2021-08-03 18:49:48 -0700

    facebook OG meta head tags

--- pages/_app.js
+++ pages/_app.js
@@ -8,20 +8,25 @@ import Head from "next/head";
 export default function MyApp(props) {
     const Component = props.Component
     return (
-    <div>
+    <html lang="en-US">
         <Head>
             <link rel="shortcut icon" href="/favicon/favicon.ico" />
+            <link rel="icon" href="/favicon/favicon.ico" />
             <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧪</text></svg>"/>
+            
             <title>Conner Ward</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-            <meta name="keywords" content="HTML, CSS, JavaScript"/>
             <meta charset="UTF-8"/>
             <meta name="description" content="Developer. Loves Python, CAD, Embedded C, JS, Docker, Logic Gates."/>
-            <meta name="author" content="Conner Ward"/>
+            <meta name="author" content="Conner Ward" />
+            {/* facebook meta tags */}
+            <meta property="og:title" content="Conner Ward" />
+            <meta property="og:image" content="https://developer.mozilla.org/static/img/opengraph-logo.png"/>
+            <meta property="og:description" content="UX Design, Web Dev, Embedded Software."/>
         </Head>
         <Nav props={{ props: { active: props.pageProps.active}}}></Nav>
         <Component {...props} />
         <Footer></Footer>
-    </div>
+    </html>
     )
 }
